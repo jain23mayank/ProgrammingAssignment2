287c8fdd27a2e9c1c7fbc346425cab4c9a7b32d2
Mayank Jain <jain23mayank@gmail.com>
2018-12-16 11:52:32 +0530

    updated code

--- cachematrix.R
+++ cachematrix.R
@@ -13,7 +13,7 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function()x
   setinverse<- function(solve)m<< solve
   getinverse<- function()m
-  list(set=set, get = get, setinverse= setinverse, getinverse - getinverse)
+  list(set=set, get = get, setinverse= setinverse, getinverse = getinverse)
 
 }
 
